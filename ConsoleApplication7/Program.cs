fecabfbfbacd10c911a3008337f39b79d7c0b807
Cron <Cron@HOME>
2016-06-24 13:29:33 +0300

    Только TF

--- ConsoleApplication7/Program.cs
+++ ConsoleApplication7/Program.cs
@@ -14,59 +14,68 @@ namespace ConsoleApplication7
     {
         static void Main(string[] args)
         {
-            var webClient = new WebClient();
-            webClient.Encoding = Encoding.GetEncoding(1251);
-            var textOriginal = webClient.DownloadString(
-                "http://lib.ru/KING/r_king-33.txt");
+            CalculateTF();
+        }
 
-            var regexSentences = new Regex("[А-Яа-я0-9\\)\\(\\,+\\.+\\:+\\-+\"+\\s+\\?+\\!+]+(?!\\?\\!\\.\\s+[А-Я]+)");
+        public static void CalculateTF()
+        {
+            SortedDictionary<string, int> dicWords = new SortedDictionary<string, int>();
+            foreach (string word in GetWords())
+            {
+                int count;
+                dicWords[word] = dicWords.TryGetValue(word, out count) ? count + 1 : 1;
+            }
+            
+            foreach (KeyValuePair<string, int> pair in dicWords)
+            {
+                double tf;
+                tf = pair.Value / (double)dicWords.Count();
+                Console.WriteLine("{1,8} {0}", pair.Key, tf);
+            }
+        }
+
+        
+        public static IEnumerable<string> GetSentences()
+        {
+            Encoding enc = Encoding.GetEncoding(1251);
+            var textOriginal = File.ReadAllText("1.txt", enc);
+
+            var regexSentences = new Regex("[А-Я].*?[.!?]\\s+(?=[А-Я])");
             var sentences =
                 regexSentences.Matches(textOriginal)
                     .OfType<Match>()
                     .Select(match => match.Value)
-            //.Select(s => s.Trim())
-            //.Select(x => x.ToLower())
             .ToList();
 
-            var listSentences = sentences
-            //        .GroupBy(s => s)
-            //        .OrderByDescending(g => g.Count())
-            //        .Select(g => new
-            //        {
-            //            Word = g.Key,
-            //            P = g.Count() * 1.0 / words.Count
-            //        })
-                    //.Take(50)
-                    .ToArray();
-
-            foreach (var item in listSentences)
+            foreach (var item in sentences)
             {
-                File.WriteAllLines("textSentences.txt", listSentences);
+                File.WriteAllLines("textSentences.txt", sentences);
             }
+            return sentences;
+        }
 
+        public static IEnumerable<string> GetWords()
+        {
+            GetSentences();
             var textSentences = File.ReadAllText("textSentences.txt");
             var regexWords = new Regex("\\w+(?=\\W)");
             var words =
                 regexWords.Matches(textSentences)
                     .OfType<Match>()
                     .Select(match => match.Value)
-                    //.Select(s => s.Trim())
-                    //.Select(x => x.ToLower())
                     .ToList();
 
-            var list = words
-                            
-                    .ToArray();
+            int wordsQuantity = words.Count();
 
-            int wordsQuantity = list.Count();
-
-            foreach (var item in list)
+            foreach (var item in words)
             {
-                File.WriteAllLines("textWords.txt", list);
+                File.WriteAllLines("textWords.txt", words);
             }
-            
+
             Console.WriteLine("Слов в тексте - {0}", wordsQuantity);
 
+            return words;
         }
     }
 }
+
